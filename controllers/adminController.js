1c4daf4ff0dfda77f19781778627db3522f3f301
anil8c7 <anil8c7@gmail.com>
2023-11-05 19:04:15 +0530

    env.example added

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,7 +6,7 @@ router.get('/signin', async (req, resp) => {
     resp.render('admin/signIn');
 })
 router.get('/signup', async (req, resp) => {
-   
+
     resp.render('admin/signUp');
 })
 router.post('/signin', async (req, resp) => {
@@ -18,34 +18,47 @@ router.post('/signup', async (req, resp) => {
         status: 400, // Default to an error status
         message: '' // Initialize the message
     };
-    console.log("ddd"+name)    
-    console.log(email)    
-    console.log(password)    
     try {
-        if(name!=="" && email !=="" && password!==""){
-            const newUser = await userModel.createUser(name, email, password);
+        if (name !== "" && email !== "" && password !== "") {
+          let isEmailValid = await checkEmailValidation(email)
+            if (isEmailValid) {
+                const newUser = await userModel.createUser(name, email, password);
+                const data = {
+                    status: 400, // Default to an error status
+                    message: 'User Created' // Initialize the message
+                };
+                resp.render('admin/signUp', data);
+            } else {
+                const data = {
+                    status: 400,
+                    message: "Email is not Valid"
+                }
+                resp.render('admin/signUp', data);
+            }
+        }
+        else {
             const data = {
-                status: 400, // Default to an error status
-                message: 'User Created' // Initialize the message
-            };
-           resp.render('admin/signUp',data);
+                status: 400,
+                message: "All fields are required"
+            }
+            resp.render('admin/signUp', data);
         }
-      else{
-        const data = {
-            status:400,
-            message:"All fields are required"
-        }
-        resp.render('admin/signUp',data);
-      }    
     }
     catch (error) {
         const data = {
-            status: 500, 
+            status: 500,
             message: error.message // Initialize the message
-        };    
-        resp.render('admin/signUp',data);
+        };
+        resp.render('admin/signUp', data);
     }
-
 })
 
+
+function checkEmailValidation(email) {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(email)) {
+        return false;
+    }
+    return true
+}
 module.exports = router;
