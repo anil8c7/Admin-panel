3dd83b8f2b6ef6269aa47cec753b8380ce943c3d
anil8c7 <anil8c7@gmail.com>
2023-12-23 13:42:56 +0530

    added data

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express  = require('express')
 const router  =  express.Router();
 const app = express();
 const bodyParser =  require('body-parser');
-const routes =  require('./routes/routes');
 const config = require('./config/config')
+const routes =  require('./routes/admin');
+// admin routes
+app.use('/admin',routes);
 app.set('view engine', 'ejs');
 app.use(express.static('assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use('/',routes);
 const port = config.server.PORT;
 app.listen(port,(err)=>{
     if(err){
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const adminRoutes = require('../controllers/adminController');
 
-router.use('/admin', adminRoutes);
+router.use('/', adminRoutes);
 
 module.exports = router;
